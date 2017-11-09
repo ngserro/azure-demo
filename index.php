5fc0ecf03605aad02f67f48f96652829501a3330
Nuno Serro <ngserro@gmail.com>
2017-11-09 17:38:42 +0000

    version 2

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Azure App Service Demo</p>'; 
+<?php
+
+echo '<p>Azure App Service Demo</p>'; 
+echo '<p>This is version 2.0</p>'; 
 
 
 ?>
